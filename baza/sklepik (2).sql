055c93fcc37af940fffa9333f0cc44e92cba6f30
ykarolina <kacieniecka2@gmail.com>
2026-05-02 03:01:55 +0200

    baza z danymi do testow

---
+++ baza/sklepik (2).sql
@@ -0,0 +1,208 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Maj 02, 2026 at 03:01 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sklepik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pozycje_zamowione`
+--
+
+CREATE TABLE `pozycje_zamowione` (
+  `id` int(11) NOT NULL,
+  `id_zamowienia` int(11) NOT NULL,
+  `id_produktu` int(11) NOT NULL,
+  `ilosc` int(11) NOT NULL,
+  `cena` decimal(3,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pozycje_zamowione`
+--
+
+INSERT INTO `pozycje_zamowione` (`id`, `id_zamowienia`, `id_produktu`, `ilosc`, `cena`) VALUES
+(3, 2, 3, 2, 5.00),
+(4, 2, 4, 1, 4.00),
+(7, 3, 4, 2, 2.50);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `produkty`
+--
+
+CREATE TABLE `produkty` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(50) NOT NULL,
+  `cena` decimal(5,2) NOT NULL,
+  `zdjecie` varchar(40) NOT NULL,
+  `kategoria` varchar(30) NOT NULL,
+  `smak` varchar(20) DEFAULT NULL,
+  `czy_promocja` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produkty`
+--
+
+INSERT INTO `produkty` (`id`, `nazwa`, `cena`, `zdjecie`, `kategoria`, `smak`, `czy_promocja`) VALUES
+(1, 'bułka z ser', 3.50, 'bulka_ser.png', 'bulki', NULL, 0),
+(2, 'espresso', 1.50, 'espresso.png', 'kawy', NULL, 1),
+(3, 'tymbark karton mango', 4.00, 'sok_karton_mango.jpg', 'napoje', 'mango', 0),
+(4, 'tost z szynka', 2.50, 'tost_szynka.jpg', 'na_cieplo', NULL, 0),
+(5, 'aaa', 2.00, 'a', 'kawa', NULL, 0),
+(6, 'tymbark', 9.99, 'aa.jpg', 'napoje', 'jablko mieta', 1),
+(7, 'biała11', 9.99, 'a', 'kawa', 'jablko mieta', 0),
+(8, 'aa', 9.99, 'aa', 'kawa', NULL, 0),
+(9, 'dsa', 9.99, 'dasds', 'kawa', NULL, 0),
+(12, 'aaa', 2.00, 'aa', 'kawa', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `uzytkownicy`
+--
+
+CREATE TABLE `uzytkownicy` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `haslo` varchar(255) NOT NULL,
+  `rola` varchar(20) NOT NULL,
+  `kod_dostepu` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id`, `nazwa`, `email`, `haslo`, `rola`, `kod_dostepu`) VALUES
+(2, 'admin1', 'admin@gmail.com', 'haslo12345', 'admin', 123456789),
+(3, 'user2', 'user2@gmail.com', 'haslo1111', 'user', NULL),
+(21, 'igi1', 'igi@aa', '$2y$10$5FGESNI02Tw1uoIzE04z3.ZsBv2rfBuiFEgCm88a/8PA4joMBKVrC', 'user', NULL),
+(22, 'admint', 'aa@q', '$2y$10$5FGESNI02Tw1uoIzE04z3.ZsBv2rfBuiFEgCm88a/8PA4joMBKVrC', 'admin', 123);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zamowienia`
+--
+
+CREATE TABLE `zamowienia` (
+  `id` int(11) NOT NULL,
+  `id_uzytkownika` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `kwota` decimal(3,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `zamowienia`
+--
+
+INSERT INTO `zamowienia` (`id`, `id_uzytkownika`, `status`, `kwota`) VALUES
+(2, 2, 'zrealizowane', 9.00),
+(3, 21, 'oczekujace', 5.00);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `pozycje_zamowione`
+--
+ALTER TABLE `pozycje_zamowione`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `pozycje_zamowione_fk1` (`id_zamowienia`),
+  ADD KEY `pozycje_zamowione_fk2` (`id_produktu`);
+
+--
+-- Indeksy dla tabeli `produkty`
+--
+ALTER TABLE `produkty`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indeksy dla tabeli `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indeksy dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `zamowienia_fk1` (`id_uzytkownika`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pozycje_zamowione`
+--
+ALTER TABLE `pozycje_zamowione`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `produkty`
+--
+ALTER TABLE `produkty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `uzytkownicy`
+--
+ALTER TABLE `uzytkownicy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pozycje_zamowione`
+--
+ALTER TABLE `pozycje_zamowione`
+  ADD CONSTRAINT `pozycje_zamowione_fk1` FOREIGN KEY (`id_zamowienia`) REFERENCES `zamowienia` (`id`),
+  ADD CONSTRAINT `pozycje_zamowione_fk2` FOREIGN KEY (`id_produktu`) REFERENCES `produkty` (`id`);
+
+--
+-- Constraints for table `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  ADD CONSTRAINT `zamowienia_fk1` FOREIGN KEY (`id_uzytkownika`) REFERENCES `uzytkownicy` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
